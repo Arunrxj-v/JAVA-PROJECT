2c933de2b5dc6c71b6efe612f005f425913fd1a1
RJWOLVI <118201021+Riju-666@users.noreply.github.com>
2023-02-15 21:29:00 +0530

    change1

--- project.java
+++ project.java
@@ -1,16 +1,5 @@
 
 //test2
 //test3
-<<<<<<< HEAD
 //hello
-=======
-//sample
-//riju
-//test4
-//test5
-//iam gustavo 
-//rj
-//arunrajv
-//rj
-//
->>>>>>> a439c4b762f95cbc1a9c63102068e5d0bb53fa0b
+//haii
