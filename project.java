d0e0d0defba70af635bd5925618ea4b8f5549968
Arunraj v <120666350+Arunrxj-v@users.noreply.github.com>
2023-02-16 07:36:08 +0530

    3g poya code

--- project.java
+++ project.java
@@ -1,5 +1,31 @@
+class project
+{
+    String member1="Arunraj.v";
+}
+class team extends project
+{
+    String member2="Aravind R";
 
-//test2
-//test3
-//hello
-//haii
+}
+class people extends team
+{
+    String member3="Riju Mathew";
+}
+class human extends people
+{
+    String member4="Aravind Kumar V";
+}
+class man extends human
+{
+    String member5="Joel Koshy John";
+
+    public static void main(String[] args) {
+        
+        man obj=new man();
+
+      String  Team_members="member1"+"member2"+"member3"+"member4"+"member5";
+
+      System.out.println("Team members are "+Team_members);
+    }
+
+}       
