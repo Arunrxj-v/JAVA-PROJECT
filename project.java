89fcd621e3bb91f106c84009b74ca7de9a53c78e
Arunraj v <120666350+Arunrxj-v@users.noreply.github.com>
2023-02-16 15:08:32 +0530

    done

--- project.java
+++ project.java
@@ -1,29 +1,29 @@
 class project
 {
-    String member1="Arunraj.v";
+    String member1="Arunraj.v ,";
 }
 class team extends project
 {
-    String member2="Aravind R";
+    String member2="Aravind R ,";
 
 }
 class people extends team
 {
-    String member3="Riju Mathew";
+    String member3="Riju Mathew ,";
 }
 class human extends people
 {
-    String member4="Aravind Kumar V";
+    String member4="Aravind Kumar V ,";
 }
 class man extends human
 {
-    String member5="Joel Koshy John";
+    String member5="Joel Koshy John ,";
 
     public static void main(String[] args) {
         
         man obj=new man();
 
-      String  Team_members="member1"+"member2"+"member3"+"member4"+"member5";
+      String  Team_members=obj.member1+obj.member2+obj.member3+obj.member4+obj.member5;
 
       System.out.println("Team members are "+Team_members);
     }
